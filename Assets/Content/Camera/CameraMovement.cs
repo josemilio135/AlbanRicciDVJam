f21027c0bc8a6a42e4191511f2afa1643e93a71e
José Albán <jose.alban@davinci.edu.ar>
2025-09-26 01:40:40 -0300

    Dialogue System

--- Assets/Content/Camera/CameraMovement.cs
+++ Assets/Content/Camera/CameraMovement.cs
@@ -1,29 +1,60 @@
-using System.Collections;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     Player _player;
-
-    [SerializeField] Vector3 _offset = new Vector3(0, 5, -2.5f);
     [SerializeField] float _smoothSpeed = 5f;
+    [SerializeField] float _lookSmoothSpeed = 2.5f;
+
+    public Vector3 _currentOffset; //quitar luego el public de ambos, ahora es para testeo
+    public Vector3 _dialoguePos;
+
+    Transform _lookTarget;
+    bool _isDialogueMode = false;
+
+    Vector3 _playerOffset = new Vector3(0, 5, -2.5f);
+    Vector3 _dialogueOffset = new Vector3(0, 1.75f, -5f);
 
     void Awake()
     {
         _player = FindAnyObjectByType<Player>();
+        FinishDialogueCam();
     }
 
     void LateUpdate()
     {
         if (_player == null) return;
 
+        if (_isDialogueMode)
+        {
+            transform.position = Vector3.Lerp(transform.position, _dialoguePos, _smoothSpeed * Time.deltaTime);
 
-        Vector3 _dir = _player.transform.position + _offset;
-     
-        transform.position = Vector3.Lerp(transform.position, _dir, _smoothSpeed * Time.deltaTime);
-
-        transform.LookAt(_player.transform.position);
+            Quaternion _targetRot = Quaternion.LookRotation(_lookTarget.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, _targetRot, _lookSmoothSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 playerTargetPos = _player.transform.position + _currentOffset;
+            transform.position = Vector3.Lerp(transform.position, playerTargetPos, _smoothSpeed * Time.deltaTime);
+            transform.LookAt(_player.transform.position);
+        }
     }
-    
+
+    public void StartDialogueCam(Transform _newTarget)
+    {
+        _isDialogueMode = true;
+        _dialoguePos = _newTarget.position + _dialogueOffset;
+        _lookTarget = _newTarget;
+    }
+    public void ChangeLookCam(Transform _newLookTarget)
+    {
+        _lookTarget = _newLookTarget;
+    }
+    public void FinishDialogueCam()
+    {
+        _currentOffset = _playerOffset;
+        _isDialogueMode = false;
+    }
+
 }
 
--- Assets/Content/NPCs/NPC.cs
+++ Assets/Content/NPCs/NPC.cs
@@ -4,38 +4,56 @@ using TMPro;
 using UnityEngine;
 public class NPC : MonoBehaviour
 {
-    [SerializeField] TextMeshPro _text;
+    [Header("Texts")]
+    [SerializeField] TextMeshPro _textNPC;
+    TextMeshPro _textPlayer;
+    [Header("Explotions")]
     [SerializeField] List<GameObject> _explotions = new List<GameObject>();
     public ShakeData _explotionShakeData;
+    [Header("Dialogue")]
+    [SerializeField] List<DialogueBlock> _dialogueBlocks = new List<DialogueBlock>();
+    int _blockIndex = 0;
+    int _lineIndex = 0;
+    bool _isInDialogue = false;
+
     Player _player;
+    CameraMovement _camera;
+
     float _lifeTime = 30;
     bool _willExplote = false;
     void Start()
     {
         _player = FindAnyObjectByType<Player>();
-        _text.enabled = false;
+        _camera = FindAnyObjectByType<CameraMovement>();
+        _textPlayer = _player.GetComponentInChildren<TextMeshPro>();
+        _textPlayer.enabled = false;
+        _textNPC.enabled = false;
     }
 
     void Update()
     {
         float _distance = Vector3.Distance(transform.position, _player.transform.position);
 
-        if (_distance < 1)
+        if (_distance < 1 && !_isInDialogue && !_willExplote)
         {
-            _text.enabled = true;
-            if (Input.GetKey(KeyCode.E))
+            _textNPC.enabled = true;
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                _willExplote = true;
+                _isInDialogue = true;
+                if (_blockIndex == 0) _camera.StartDialogueCam(transform);
+                _lineIndex = 0;
+                ShowLine();
             }
-           
         }
-        else if (!_willExplote)_text.enabled = false;
+        else if (_isInDialogue && Input.GetKeyDown(KeyCode.E)) NextLine();
+        else if (!_willExplote && !_isInDialogue) _textNPC.enabled = false;
 
-        if (_willExplote && _lifeTime >= 0)
+
+        if (_willExplote && _lifeTime > 0)
         {
 
             _lifeTime -= Time.deltaTime;
-            _text.text = _lifeTime.ToString("00");
+            _textNPC.text = _lifeTime.ToString("00");
         }
 
         if (_lifeTime <= 0 || Input.GetKey(KeyCode.F) && _willExplote)
@@ -45,5 +63,67 @@ public class NPC : MonoBehaviour
             CameraShakerHandler.Shake(_explotionShakeData);
             Destroy(gameObject);
         }
+        if (_textPlayer.enabled)
+        {
+            Vector3 _cameraPos = _camera.transform.position - _textPlayer.transform.position;
+            _cameraPos.y = 0;
+            _textPlayer.transform.rotation = Quaternion.LookRotation(-_cameraPos);
+        }
+
+    }
+    void NextLine()
+    {
+        _lineIndex++;
+
+        if (_lineIndex < _dialogueBlocks[_blockIndex]._lines.Count)
+        {
+            ShowLine();
+        }
+        else
+        {
+            _blockIndex++;
+            _lineIndex = 0;
+
+            if (_blockIndex < _dialogueBlocks.Count) ShowLine();
+            else
+            {
+                _isInDialogue = false;
+                _willExplote = true;
+                _textNPC.enabled = true;
+                _textPlayer.enabled = false;
+                _camera.FinishDialogueCam();
+            }
+        }
+    }
+    void ShowLine()
+    {
+        DialogueBlock currentBlock = _dialogueBlocks[_blockIndex];
+
+        string _line = _dialogueBlocks[_blockIndex]._lines[_lineIndex];
+
+        if (currentBlock.speaker == DialogueBlock.Speaker.NPC)
+        {
+            _textPlayer.enabled = false;
+            _textNPC.enabled = true;
+            _textNPC.text =  _line;
+            _camera.ChangeLookCam(transform);
+        }
+        else //player
+        {
+            _textNPC.enabled = false;
+            _textPlayer.enabled = true;
+            _camera.ChangeLookCam(_player.transform);
+            _textPlayer.text = _line;
+        }
+    }
+
+    [System.Serializable]
+    public class DialogueBlock
+    {
+        public enum Speaker { NPC, Player }
+        public Speaker speaker;
+
+        [TextArea]
+        public List<string> _lines = new List<string>();
     }
 }
